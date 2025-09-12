dcaf702f9a823a33e90ebbd8dc3c2d448e75b251
milim <milimon4ikyt@gmail.com>
2025-09-12 18:02:34 -0400

    commit

--- src/main/java/me/jetby/treex/actions/ActionExecutor.java
+++ src/main/java/me/jetby/treex/actions/ActionExecutor.java
@@ -1,9 +1,6 @@
 package me.jetby.treex.actions;
 
 import lombok.experimental.UtilityClass;
-import me.jetby.treex.text.Colorize;
-import me.jetby.treex.text.Papi;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -13,22 +10,25 @@ import java.util.Map;
 public class ActionExecutor {
 
     public void execute(@NotNull ActionContext ctx,
-                        @NotNull Map<ActionType, List<String>> actions) {
+                        @NotNull Map<String, List<String>> actions) {
 
         for (var entry : actions.entrySet()) {
-            ActionType type = entry.getKey();
+            String actionName = entry.getKey();
             List<String> contexts = entry.getValue();
+
+            Action action = null;
+            ActionType type = ActionType.getType(actionName);
+            if (type != null) action = type.getAction();
+
+            if (action == null) {
+                action = ActionTypeRegistry.get(actionName);
+            }
+
+            if (action == null) continue;
 
             for (String message : contexts) {
                 ctx.put("message", message);
-
-                var action = type.getAction();
-                if (action != null) {
-                    action.execute(ctx);
-                } else {
-                    var custom = ActionTypeRegistry.get(type.name());
-                    if (custom != null) custom.execute(ctx);
-                }
+                action.execute(ctx);
             }
         }
     }
--- src/main/java/me/jetby/treex/actions/ActionRegistry.java
+++ src/main/java/me/jetby/treex/actions/ActionRegistry.java
@@ -5,20 +5,17 @@ import me.jetby.treex.Treex;
 import me.jetby.treex.tools.LogInitialize;
 import me.jetby.treex.tools.log.Logger;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
 @UtilityClass
 public class ActionRegistry {
     private final Pattern ACTION_PATTERN = Pattern.compile("\\[(\\S+)] ?(.*)");
-
     private static final Logger logger = LogInitialize.getLogger(Treex.class);
 
-    public Map<ActionType, List<String>> transform(List<String> settings) {
-        Map<ActionType, List<String>> actions = new HashMap<>();
+    public Map<String, List<String>> transform(List<String> settings) {
+        Map<String, List<String>> actions = new LinkedHashMap<>();
+
         for (String s : settings) {
             var matcher = ACTION_PATTERN.matcher(s);
             if (!matcher.matches()) {
@@ -26,16 +23,13 @@ public class ActionRegistry {
                 continue;
             }
 
-            var type = ActionType.getType(matcher.group(1).toUpperCase());
-            if (type == null) {
-                logger.warn("ActionType " + s + " is not available!");
-                continue;
-            }
+            String actionName = matcher.group(1).toUpperCase();
+            String context = matcher.group(2).trim();
 
-            var context = matcher.group(2).trim();
-            actions.putIfAbsent(type, new ArrayList<>());
-            actions.get(type).add(context);
+            actions.putIfAbsent(actionName, new ArrayList<>());
+            actions.get(actionName).add(context);
         }
+
         return actions;
     }
 }
