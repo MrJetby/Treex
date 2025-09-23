d42518183262273ebcbecc92b52ffcd9409f151e
milim <milimon4ikyt@gmail.com>
2025-09-22 22:37:08 -0400

    Added LocationGenerator and WGHook

--- src/main/java/me/jetby/treex/Treex.java
+++ src/main/java/me/jetby/treex/Treex.java
@@ -1,6 +1,10 @@
 package me.jetby.treex;
 
+import me.jetby.treex.tools.LogInitialize;
+import me.jetby.treex.tools.log.Logger;
+import me.jetby.treex.worldguard.WGHook;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public final class Treex extends JavaPlugin {
 
@@ -8,15 +12,22 @@ public final class Treex extends JavaPlugin {
     public static Treex getInstance() {
         return INSTANCE;
     }
+    public static final Logger LOGGER = LogInitialize.getLogger("EvilMobs");
 
     @Override
     public void onEnable() {
         INSTANCE = this;
 
+        WGHook.init();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static void init(JavaPlugin plugin) {
+        LOGGER.success(plugin.getName() + " has been registered");
+    }
+
 }
